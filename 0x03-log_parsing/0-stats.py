576b7a16fcc461baf56fc17d0dd64a59a926dbd0
amanuelgthn <amanuelgthn@gmail.com>
2023-12-20 10:35:54 +0100

    update 0-stats.py

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -27,7 +27,7 @@ try:
             code_value[status_code] += 1
         sum_file_size += file_size
         count += 1
-        if count == 9:
+        if count == 10:
             print("File size: {}".format(sum_file_size))
             sorted_code = dict(sorted(code_value.items()))
             for k, v in sorted_code.items():
