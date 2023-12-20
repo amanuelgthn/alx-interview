25f2d95f5e73d66335026944b213936b28d4b291
amanuelgthn <amanuelgthn@gmail.com>
2023-12-20 11:27:58 +0100

    Update 0-stats.py

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -9,7 +9,8 @@ import sys
 
 possible_codes = [200, 301, 400, 401, 403, 404, 405, 500]
 sum_file_size = 0
-code_value = {"200": 0, "301": 0, "400": 0, "401": 0, "403": 0, "404": 0, "405": 0, "500": 0}
+code_value = {"200": 0, "301": 0, "400": 0, "401": 0,
+              "403": 0, "404": 0, "405": 0, "500": 0}
 count = 0
 try:
     for line in sys.stdin:
@@ -22,8 +23,8 @@ try:
             pass
         if status_code in code_value.keys():
             code_value[status_code] += 1
-        sum_file_size += file_size
-        count += 1
+            sum_file_size += file_size
+            count += 1
         if count % 10 == 0:
             print("File size: {}".format(sum_file_size))
             for k in sorted(code_value.keys()):
@@ -33,6 +34,6 @@ try:
 except KeyboardInterrupt as e:
     print("File size: {}".format(sum_file_size))
     for k in sorted(code_value.keys()):
-                if code_value[k] != 0:
-                    print("{}: {}".format(k, code_value[k]))
+        if code_value[k] != 0:
+            print("{}: {}".format(k, code_value[k]))
     raise
