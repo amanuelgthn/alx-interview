2436bc09d7fd7170387445c044b1cc22e00103c9
amanuelgthn <amanuelgthn@gmail.com>
2023-12-20 11:24:50 +0100

    Update 0-stats.py

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -9,32 +9,30 @@ import sys
 
 possible_codes = [200, 301, 400, 401, 403, 404, 405, 500]
 sum_file_size = 0
-code_value = {200: 0, 301: 0, 400: 0, 401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
+code_value = {"200": 0, "301": 0, "400": 0, "401": 0, "403": 0, "404": 0, "405": 0, "500": 0}
 count = 0
-sorted_code = {}
 try:
     for line in sys.stdin:
         try:
             if len(line.split()) != 9:
                 pass
-            status_code = int(line.split()[7])
+            status_code = line.split()[7]
             file_size = int(line.split()[8])
         except Exception:
             pass
-        if status_code in possible_codes:
+        if status_code in code_value.keys():
             code_value[status_code] += 1
         sum_file_size += file_size
         count += 1
         if count % 10 == 0:
             print("File size: {}".format(sum_file_size))
-            sorted_code = dict(sorted(code_value.items()))
-            for k, v in sorted_code.items():
-                if sorted_code[k] != 0:
-                    print("{}: {}".format(k, v))
+            for k in sorted(code_value.keys()):
+                if code_value[k] != 0:
+                    print("{}: {}".format(k, code_value[k]))
                 code_value[k] = 0
 except KeyboardInterrupt as e:
     print("File size: {}".format(sum_file_size))
-    for k, v in sorted_code.items():
-        if sorted_code[k] != 0:
-            print("{}: {}".format(k, v))
+    for k in sorted(code_value.keys()):
+                if code_value[k] != 0:
+                    print("{}: {}".format(k, code_value[k]))
     raise
