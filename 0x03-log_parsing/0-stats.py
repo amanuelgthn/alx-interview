8b895ddab80753f63b95d3acaa73d67aec343f1d
amanuelgthn <amanuelgthn@gmail.com>
2023-12-20 09:38:59 +0100

    Update 0-stats.py fix the counting variable

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -25,7 +25,7 @@ try:
             code_value[status_code] += 1
         sum_file_size += file_size
         count += 1
-        if count == 10:
+        if count == 9:
             print("File size: {}".format(sum_file_size))
             sorted_code = dict(sorted(code_value.items()))
             for k, v in sorted_code.items():
