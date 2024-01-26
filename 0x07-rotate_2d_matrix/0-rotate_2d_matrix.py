db47181808fd1a5910c6935be4f535dcaf5f7bac
amanuelgthn <amanuelgthn@gmail.com>
2024-01-26 23:15:36 +0100

    Rotate 2d matrix

---
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+Rotate 2D Matrix
+"""
+
+
+def rotate_2d_matrix(matrix):
+    for i in range(0, len(matrix)):
+        for j in range(0, len(matrix) - 1):
+            matrix[i][j + 1], matrix[j + 1][i] = matrix[j + 1][i], matrix[i][j + 1]
+    print(matrix)
+    for i in range(0, len(matrix)):
+        for j in range(0, len(matrix)// 2):
+            matrix[i][j], matrix[i][len(matrix) - j - 1] = matrix[i][len(matrix) - j - 1],  matrix[i][j]
