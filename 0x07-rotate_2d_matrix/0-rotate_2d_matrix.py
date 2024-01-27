9271d61de88ab650a217cdd7c56205595fcd0bd6
amanuelgthn <amanuelgthn@gmail.com>
2024-01-27 23:06:00 +0100

    Update rotate_2d_matrix

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -6,9 +6,10 @@ Rotate 2D Matrix
 
 def rotate_2d_matrix(matrix):
     for i in range(0, len(matrix)):
-        for j in range(0, len(matrix) - 1):
-            matrix[i][j + 1], matrix[j + 1][i] = matrix[j + 1][i], matrix[i][j + 1]
-    print(matrix)
+        for j in range(i, len(matrix) - 1):
+            matrix[j + 1][i], matrix[i][j + 1] =\
+                matrix[i][j + 1], matrix[j + 1][i]
     for i in range(0, len(matrix)):
-        for j in range(0, len(matrix)// 2):
-            matrix[i][j], matrix[i][len(matrix) - j - 1] = matrix[i][len(matrix) - j - 1],  matrix[i][j]
+        for j in range(0, len(matrix) // 2):
+            matrix[i][j], matrix[i][len(matrix) - j - 1] =\
+                matrix[i][len(matrix) - j - 1],  matrix[i][j]
