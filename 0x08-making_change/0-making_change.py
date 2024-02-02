9adf6a11608da5d0ed4b1d3ca3244dddcb88f50c
amanuelgthn <amanuelgthn@gmail.com>
2024-02-02 02:13:43 +0100

    Update 0-making_change.py

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -10,6 +10,17 @@ def makeChange(coins, total):
     Function to determine the fewest number of coins needed to meet a
     given amount total
     """
-
+    if total <= 0:
+        return 0
+    check = 0
+    temp = 0
     coins.sort(reverse=True)
-    print(coins)
+    for i in coins:
+        while check < total:
+            check += i
+            temp += 1
+        if check == total:
+            return temp
+        check -= i
+        temp -= 1
+    return -1
