0345392f954888dea13205126e366e16bad54add
amanuelgthn <amanuelgthn@gmail.com>
2024-02-14 23:54:35 +0100

    0-prime_game.py

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -2,7 +2,18 @@
 """prime_game"""
 
 
-
 def isWinner(x, nums):
-    """function that reutrns the name of that won the most rounds"""
-    
+    """function that returns the name of that won the most rounds"""
+    maria = 0
+    ben = 0
+    for num in nums:
+        j = 2
+        if num == 1:
+            ben += 1
+        if num == 2:
+            maria += 1
+        for number in nums:
+            if number // num == 0:
+                nums.pop(number)
+        
+ 
